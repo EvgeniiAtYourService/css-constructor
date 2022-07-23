80469ec7ceb4ee049893267e66476c07a231b42b
EvgeniiAtYourService <eugeniotorneros@gmail.com>
2022-07-23 14:39:26 +0500

    Add search field & x-mark

--- src/components/Properties/PropertiesWindow.tsx
+++ src/components/Properties/PropertiesWindow.tsx
@@ -4,7 +4,14 @@ import styles from './PropertiesWindow.module.css'
 
 const PropertiesWindow = ({ propertiesWindowShown, showPropertiesWindow }: IPropertiesWindowProps) => {
   if (propertiesWindowShown) {
-    return <div className={styles['properties-window']}>Properties</div>
+    return (
+      <div className={styles['properties-window']}>
+        <div className={styles['search-field-wrapper']}>
+          <span className={styles['x']}>&#10005;</span>
+          <input type="text" className={styles['search-field']} />
+        </div>
+      </div>
+    )
   }
   return <>13</>
 }
